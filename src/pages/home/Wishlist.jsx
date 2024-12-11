ee64b174aed01c27572aa91e09cdb407a210a17b
Zion <omogbemezion@gmail.com>
2024-12-11 15:40:19 +0100

    wishlist done

--- src/pages/home/Wishlist.jsx
+++ src/pages/home/Wishlist.jsx
@@ -3,6 +3,8 @@ import Header from './header'
 import product1 from '../../assets/product1.jpg'
 import { useState } from 'react'
 import { FaRegTrashAlt } from "react-icons/fa";
+import product2 from '../../assets/product2.jpg'
+import product4 from '../../assets/product4.jpg'
 const Wishlist = () => {
     const [rating, setRating] = useState(0); // Store the rating
 
@@ -14,7 +16,7 @@ const Wishlist = () => {
         <Header/> 
         <section className='wishlist'>
            <h1 className="my">My Wishlist</h1>
-           <p className="products">There are  <span className="five">5 </span>products in this list</p>
+           <p className="products">There are  <span className="five">3 </span>products in this list</p>
 
            <table className="main-table">
               <thead>
@@ -46,6 +48,50 @@ const Wishlist = () => {
                   <td className="price">$2.51</td>
                   <td><span className="stock">In Stock</span></td>
                   <td><a href="" className="todo">Add to cart</a></td>
+                  <td><a href="" className="trash"><FaRegTrashAlt /></a></td>
+                </tr>
+
+                <tr className="each">
+                  <td> <img src={product2} alt="" className='product-image'/></td>
+                  <td className="describe">
+                    <a href="" className="link">Blue Diamond Almonds Lightly Salted</a>
+                    <div>
+                    {[...Array(5)].map((_, index) => (
+                            <span
+                              key={index}
+                              className={index < rating ? "star filled" : "star"}
+                              onClick={() => handleRating(index + 1)} // Set rating on click
+                            >
+                              ★
+                            </span>
+                          ))}
+                    </div>        
+                  </td>
+                  <td className="price">$3.2</td>
+                  <td><span className="stock">In Stock</span></td>
+                  <td><a href="" className="todo">Add to cart</a></td>
+                  <td><a href="" className="trash"><FaRegTrashAlt /></a></td>
+                </tr>
+
+                <tr className="each">
+                  <td> <img src={product4} alt="" className='product-image'/></td>
+                  <td className="describe">
+                    <a href="" className="link">Field Roast Chao Cheese Creamy Original</a>
+                    <div>
+                    {[...Array(5)].map((_, index) => (
+                            <span
+                              key={index}
+                              className={index < rating ? "star filled" : "star"}
+                              onClick={() => handleRating(index + 1)} // Set rating on click
+                            >
+                              ★
+                            </span>
+                          ))}
+                    </div>        
+                  </td>
+                  <td className="price">$3.21</td>
+                  <td><span className="stocks">Out Stock</span></td>
+                  <td><a href="" className="todos">Contact Us</a></td>
                   <td><a href="" className="trash"><FaRegTrashAlt /></a></td>
                 </tr>
               </tbody>
