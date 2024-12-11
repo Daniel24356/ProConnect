d17e0e35980db8cae5d59ecb2d415f1b38858d31
Zion <omogbemezion@gmail.com>
2024-12-11 01:49:16 +0100

    main-design done

--- src/pages/home/Wishlist.jsx
+++ src/pages/home/Wishlist.jsx
@@ -1,6 +1,14 @@
 import './Wishlist.css'
 import Header from './header'
+import product1 from '../../assets/product1.jpg'
+import { useState } from 'react'
+import { FaRegTrashAlt } from "react-icons/fa";
 const Wishlist = () => {
+    const [rating, setRating] = useState(0); // Store the rating
+
+    const handleRating = (rate) => {
+      setRating(rate); // Update the rating state
+    };
   return (
       <>
         <Header/> 
@@ -18,6 +26,29 @@ const Wishlist = () => {
                     <th scope='col'>Remove</th>
                 </tr>
               </thead>
+              <tbody>
+                <tr className="each">
+                  <td> <img src={product1} alt="" className='product-image'/></td>
+                  <td className="describe">
+                    <a href="" className="link">Field Roast Chao Cheese Creamy Original</a>
+                    <div>
+                    {[...Array(5)].map((_, index) => (
+                            <span
+                              key={index}
+                              className={index < rating ? "star filled" : "star"}
+                              onClick={() => handleRating(index + 1)} // Set rating on click
+                            >
+                              ★
+                            </span>
+                          ))}
+                    </div>        
+                  </td>
+                  <td className="price">$2.51</td>
+                  <td><span className="stock">In Stock</span></td>
+                  <td><a href="" className="todo">Add to cart</a></td>
+                  <td><a href="" className="trash"><FaRegTrashAlt /></a></td>
+                </tr>
+              </tbody>
            </table>
         </section>
      </>
