7cdec39862a4c5007536da5bbd3db0ccfcb65931
Zion <omogbemezion@gmail.com>
2024-12-19 18:37:46 +0100

    finished responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import './App.css'
-// import Home from './pages/home/home'
+import Home from './pages/home/home'
 // import Wishlist from './pages/home/Wishlist'
 // import Comparable from './pages/home/Comparable'
 // import Login from './pages/home/Login'
-import Signup from './pages/home/Signup'
+// import Signup from './pages/home/Signup'
 function App() {
 
   return (
     <>
-    {/* <Home/> */}
+    <Home/>
       {/* <Wishlist/> */}
       {/* <Comparable/> */}
       {/* <Login/> */}
-      <Signup/>
+      {/* <Signup/> */}
     </>
   )
 }
