d419578adb70bf2451899bbb1f539c776ca919bc
Zion <omogbemezion@gmail.com>
2025-01-08 13:06:34 +0100

    comparing

--- src/context/ComparableContext.jsx
+++ src/context/ComparableContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useContext, useState } from "react";
+import Swal from "sweetalert2";
 
 // Create Context
 const ComparableContext = createContext();
@@ -9,8 +10,21 @@ export const ComparableProvider = ({ children }) => {
 
   // Add product to comparison list
   const addToComparable = (product) => {
-    setComparable((prevComparable) => [...prevComparable, product]);
+    setComparable((prevComparable) => {
+      if (prevComparable.length < 4) {
+        return [...prevComparable, product];
+      } else {
+        Swal.fire({
+          icon: 'warning',
+          title: 'Limit Reached',
+          text: 'You can only compare up to 4 products. Please remove one before adding another.',
+          confirmButtonText: 'OK',
+        });
+        return prevComparable; // No change to the state
+      }
+    });
   };
+  
 
   // Remove product from comparison list
   const removeFromComparable = (id) => {
